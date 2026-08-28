f05f0b68a80fba1698d01e7210b2abbb6ed13c70
RipOff <tf9627922@gmail.com>
2026-08-27 22:04:47 -0500

    Added the ability to remeber rolls and improved how the document adds the new objects and improved form handeling

--- work.js
+++ work.js
@@ -1,3 +1,5 @@
+var id = 1
+
 function getData(form) {
     var formData = new FormData(form);
     var values = [];
@@ -7,25 +9,60 @@ function getData(form) {
    return values
 }
 
+
 function makeCap(str) {
     return String(str).charAt(0).toUpperCase() + String(str).slice(1);
 }
 
-document.getElementById("char").addEventListener("submit", function (e) {
-    e.preventDefault();
-   
-    const text = getData(e.target)
-    if (text != ""){
-        const test = document.createElement("div");
-        const text2 = document.createElement("p");
-        const butt = document.createElement("button");
-        test.classList.add("object");
-        text2.classList.add("name");
-        butt.classList.add("remove");
-        text2.textContent = makeCap(text);
-        butt.textContent = "remove";
-        test.appendChild(text2);
-        test.appendChild(butt);
-        document.getElementById("body").append(test);
-    }
+document.addEventListener("DOMContentLoaded", () => {
+    const charForm = document.getElementById("char");
+    const updateForm = document.getElementById("updateInit");
+
+    charForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+    
+        const text = makeCap(getData(e.target))
+        if (text === "") return; // makes sure that there is something in the input field
+        // creates the obects
+
+        const mainItemHTML = `
+            <div class="object" id="${id}" roll="0" index="0">
+                <p class="name">${text}</p>
+                <button class="remove-btn"">Remove</button>
+            </div>
+        `;
+       
+        const footItemHTML = `
+            <div class="scroll-item" id="${id}">
+                <label>${text}
+                    <input name="${id}" type="number" min="-2" max="30" width="10px">
+                </label>
+            </div>
+        `;
+        // adds objects to page
+        const footer = document.getElementById('foot');
+        footer.insertAdjacentHTML('beforeend', footItemHTML);
+        const header = document.getElementById('body');
+        header.insertAdjacentHTML('beforeend', mainItemHTML);
+        
+        id++;
+    });
+
+    updateForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const rolls = new FormData(e.target).entries();
+        for (const roll of rolls) {
+            
+            const up = document.getElementById(roll[0]);
+            up.setAttribute("roll", roll[1]);
+        }
+    });
+
+    document.addEventListener('click', (ev) => {
+        if (!ev.target.closest('.remove-btn')) return;
+        const items = document.querySelectorAll(`[id="${ev.target.parentNode.getAttribute('id')}"]`);
+        items[0].remove();
+        items[1].remove();
+    });
 });
