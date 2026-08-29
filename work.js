0099e804acf922ddb3ca1c69c098760bb1cf2a22
RipOff <tf9627922@gmail.com>
2026-08-28 20:50:34 -0500

    added functioanal sorting for display items

--- work.js
+++ work.js
@@ -1,5 +1,16 @@
 var id = 1
 
+// Build the expiration date string:
+var expiration_date = new Date();
+var cookie_string = '';
+expiration_date.setFullYear(expiration_date.getFullYear() + 1);
+// Build the set-cookie string:
+cookie_string = "test_cookies=true; path=/; expires=" + expiration_date.toUTCString();
+// Create or update the cookie:
+document.cookie = cookie_string;
+
+
+
 function getData(form) {
     var formData = new FormData(form);
     var values = [];
@@ -14,6 +25,26 @@ function makeCap(str) {
     return String(str).charAt(0).toUpperCase() + String(str).slice(1);
 }
 
+function orderRolls() {
+    const parent = document.getElementById("body");
+    const objects = parent.children;
+    const elements = document.createDocumentFragment();
+    
+    const indices = Array.from({ length: objects.length }, (_, i) => ({
+        index: i,
+        roll: parseInt(objects[i].getAttribute("roll"))
+    }));
+    
+    indices.sort((a, b) => b.roll - a.roll);
+    
+    indices.forEach(item => {
+        elements.appendChild(objects[item.index].cloneNode(true));
+    });
+    
+    parent.innerHTML = "";
+    parent.appendChild(elements);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const charForm = document.getElementById("char");
     const updateForm = document.getElementById("updateInit");
@@ -24,6 +55,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const text = makeCap(getData(e.target))
         if (text === "") return; // makes sure that there is something in the input field
         // creates the obects
+        charForm.children[0].value = "";
 
         const mainItemHTML = `
             <div class="object" id="${id}" roll="0" index="0">
@@ -50,13 +82,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
     updateForm.addEventListener("submit", function (e) {
         e.preventDefault();
-
         const rolls = new FormData(e.target).entries();
         for (const roll of rolls) {
             
             const up = document.getElementById(roll[0]);
             up.setAttribute("roll", roll[1]);
         }
+        const lay = document.getElementById("layout");
+        const items = lay.children
+        items.forEach(function(val){
+            
+        });
+        console.log(items)
+        orderRolls()
     });
 
     document.addEventListener('click', (ev) => {
